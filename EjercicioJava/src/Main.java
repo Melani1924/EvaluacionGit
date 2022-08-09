a3b32e8e9b074d332064a5b2087cc894a6b0142b
Melani1924 <gabrieljosvic@gmail.com>
2022-08-09 14:16:35 -0500

    "Agregamos una nueva clase"

--- EjercicioJava/src/Main.java
+++ EjercicioJava/src/Main.java
@@ -3,11 +3,17 @@ public class Main {
         //Declaramos las variables
         int num1=10;
         int num2=5;
+
+        int num3=14;
         /*Realizamos las operaciones*/
 
         System.out.println("El resultado de la suma es:"+(num1+num2));
         System.out.println("El resultado de la suma es:"+(num1-num2));
         System.out.println("El resultado de la suma es:"+(num1*num2));
         System.out.println("El resultado de la suma es:"+(num1/num2));
+
+        System.out.println("El resultado de la suma es:"+(num3+num2));
+
+
     }
 }
