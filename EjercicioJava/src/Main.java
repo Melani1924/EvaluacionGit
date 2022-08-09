cf177bda8580d02826a735e4f51e54d3ddde00a8
Melani1924 <gabrieljosvic@gmail.com>
2022-08-09 13:24:41 -0500

    Desde Ordenador

---
+++ EjercicioJava/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //Declaramos las variables
+        int num1=10;
+        int num2=5;
+        /*Realizamos las operaciones*/
+
+        System.out.println("El resultado de la suma es:"+(num1+num2));
+        System.out.println("El resultado de la suma es:"+(num1-num2));
+        System.out.println("El resultado de la suma es:"+(num1*num2));
+        System.out.println("El resultado de la suma es:"+(num1/num2));
+    }
+}
